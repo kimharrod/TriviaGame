6c78e431fdb2d7632e849029c631521640f78f7e
Kimberly Harrod <kimharrod@Kimberlys-MacBook.local>
2017-03-12 16:28:57 -0500

    Correct and Incorrect answers iterate counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 
 	var questionIndex = "";
 	var gameQuestions = [];
+	var correctAnswerSlot = "";
+	var correctAnswers = 0;
+	var correctAnswer = "";
+	var incorrectAnswers = 0;
 	
 
 	// The URL we need to query to database (get the info we want from the API):
@@ -49,8 +53,8 @@ $(document).ready(function(){
  	// Select a div to display the correct answer, by generating a random number between 1 and 4
 
 
-	var correctAnswerSlot = Math.floor((Math.random() * 3) +1);
-	var correctAnswer = apiResults[questionIndex].correct_answer;
+	correctAnswerSlot = Math.floor((Math.random() * 3) +1);
+	correctAnswer = apiResults[questionIndex].correct_answer;
 	console.log(correctAnswer);
 	$("#" + "slot" + correctAnswerSlot).html(correctAnswer);
 
@@ -68,12 +72,44 @@ $(document).ready(function(){
 
 	}
 
-
-
+	
 
 
  	});	
      
    
+     	// Set the click event for the answers on the page
+
+		$(".answer").click(isCorrect); 
+
+		// Function to determine if correct answer is clicked
+
+		function isCorrect () {
+
+		// Determine the slot number clicked on from the id attribute
+
+		var clickedSlot = $(this).attr("id");
+
+		// Finally, strip the initial characters from the id attribute, leaving only slot number
+
+		clickedSlot = clickedSlot.slice(4);
+		console.log(clickedSlot);
+		console.log(correctAnswerSlot);
+
+		if (clickedSlot == correctAnswerSlot) {
+
+			$("#winlossMessage").text("You got it!");
+			correctAnswers++;
+			$("#winlossCounter").html("Correct: " + correctAnswers + "&nbsp;&nbsp;Incorrect: " + incorrectAnswers);
+
+		} else {
+
+			$("#winlossMessage").text("Sorry, the answer was : " + correctAnswer);
+			incorrectAnswers++;
+			$("#winlossCounter").html("Correct: " + correctAnswers + "&nbsp;&nbsp;Incorrect: " + incorrectAnswers);
+			
+		}
+
+		} // end isCorrect function
 
 });
